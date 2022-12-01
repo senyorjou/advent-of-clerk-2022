71ec06b4fae9a4ffde0379c5b8328ed482f7e810
Marc Jou <smyslov@gmail.com>
2022-12-01 09:07:02 +0100

    Some markdown

--- src/advent_of_clerk/day_01.clj
+++ src/advent_of_clerk/day_01.clj
@@ -3,23 +3,25 @@
   (:require [nextjournal.clerk :as clerk]
             [clojure.string :as str]))
 
+;; ##### utility fns
 (defn parse-and-aggregate
  "Gets chunks of elf data parses and sums"
   [lines]
   (apply + (map #(Integer/parseInt  %) (str/split-lines lines))))
 
-;; Input data
+;; ##### Input data
 ;;
 (def raw-data (slurp "resources/day_01.txt"))
 (def data (map parse-and-aggregate  (str/split raw-data #"\n\n")))
 
-;; Find the max value
+;; ##### Puzzle 1. Find the max value
 (defn p1 [input]
   (apply max input))
 
-;; Find the sum of 3 max values
+;; ##### Puzzle 2. Find the sum of 3 max values
 (defn p2 [input]
   (apply + (take 3 (sort > input))))
+
 
 ;; ##### Results
 (p1 data)
