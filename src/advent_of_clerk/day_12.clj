32c61d74e301ff941c975448d5e6385d5e0a07ab
Marc Jou <smyslov@gmail.com>
2022-12-12 08:52:48 +0100

    WiP. Day 12

--- src/advent_of_clerk/day_12.clj
+++ src/advent_of_clerk/day_12.clj
@@ -1,3 +1,47 @@
 ;; # 🎄 Advent of Clerk: Day 12
 (ns advent-of-clerk.day-12
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.string :as str]))
+
+
+(def raw-data (slurp "resources/day_12.txt"))
+;; (def raw-data (slurp "resources/day_12_sample.txt"))
+(def data (str/split raw-data  #"\n"))
+
+
+(def letters "abcdefghijklmnopqrstuvwxyz")
+
+(def deltas [[1 0] [-1 0] [0 1] [0 -1]])
+
+(defn char->num [ch]
+  (case ch
+    \S {:start true :e 0}
+    \E {:end true :e (+ 2 (- (int \z) (int \a)))}
+    {:e (inc (- (int ch) (int \a)))}))
+
+
+(def area-map (reduce (fn [acc, [k, v]]
+                   (assoc acc k v))
+                 {} (for [[y row] (map-indexed vector data)
+                          [x val] (map-indexed vector row)]
+                      [[x y] (char->num val)])))
+
+
+
+(defn start-point? [[k v]]
+  "Find start point"
+  (when (:start v)
+    k))
+
+
+(defn next-mov [[x y]]
+  (for [[dx dy] deltas
+        :let [next-point [(+ x dx) (+ y dy)]]
+        :when (and (area-map next-point))]
+    next-point)
+  )
+
+
+(next-mov [0 0])
+
+(filter start-point? area-map)
