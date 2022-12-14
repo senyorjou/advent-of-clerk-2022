a66e5ec21676865534cf1eb41322a3a868d4c0b1
Marc Jou <smyslov@gmail.com>
2022-12-14 06:23:16 +0100

    Days 12 (unfinished) & 13 (finished)

--- src/advent_of_clerk/day_12.clj
+++ src/advent_of_clerk/day_12.clj
@@ -4,8 +4,8 @@
             [clojure.string :as str]))
 
 
-(def raw-data (slurp "resources/day_12.txt"))
-;; (def raw-data (slurp "resources/day_12_sample.txt"))
+;; (def raw-data (slurp "resources/day_12.txt"))
+(def raw-data (slurp "resources/day_12_sample.txt"))
 (def data (str/split raw-data  #"\n"))
 
 
@@ -15,9 +15,9 @@
 
 (defn char->num [ch]
   (case ch
-    \S {:start true :e 0}
-    \E {:end true :e (+ 2 (- (int \z) (int \a)))}
-    {:e (inc (- (int ch) (int \a)))}))
+    \S {:start true :el 0}
+    \E {:end true :el (+ 2 (- (int \z) (int \a)))}
+    {:el (inc (- (int ch) (int \a)))}))
 
 
 (def area-map (reduce (fn [acc, [k, v]]
@@ -33,15 +33,62 @@
   (when (:start v)
     k))
 
+(defn valid-move? [from to]
+   (let [el-from (get-in area-map [from :el])
+         el-to (get-in area-map [to :el])]
+     (>= el-from (dec el-to))))
 
-(defn next-mov [[x y]]
+
+
+(defn next-move [[x y]]
   (for [[dx dy] deltas
         :let [next-point [(+ x dx) (+ y dy)]]
-        :when (and (area-map next-point))]
-    next-point)
-  )
+        :when (and (area-map next-point)
+                   (valid-move? [x y] [dx dy]))]
+    next-point))
 
 
-(next-mov [0 0])
+;; (loop [to-visit (into clojure.lang.PersistentQueue/EMPTY [(filter some? (map start-point? area-map))])
+;; ;; (loop [to-visit [[0 0]]
+;;        top 0]
+;;   (println "POINTS " top)
+;;   (if (> top 3)
+;;     to-visit
+;;     (let [points (into [] (next-move (peek to-visit)))]
+;;       (recur (into (pop to-visit) points)
+;;              (inc top)))))
 
-(filter start-point? area-map)
+
+;; (loop [top 0]
+;;   (println "POINTS " top)
+;;   (if (> top 3)
+;;     top
+;;     (recur (inc top))))
+
+
+
+
+;; (into [] (next-move [0 0]))
+
+;; (valid-move? [0 3] [1 3])
+;; (area-map [0 3])
+;; (area-map [1 3])
+
+
+;; (next-move (ffirst (filter start-point? area-map)))
+
+;; (peek [[0 0][1 0] [0 1]])
+;; (into (pop [1 2 3]) [4])
+;; (into [] (next-move [0 0]))
+
+;; (filter some? (map start-point? area-map))
+;; (peek (into clojure.lang.PersistentQueue/EMPTY  [(filter some? (map start-point? area-map))]))
+;; (def a (into (pop (into clojure.lang.PersistentQueue/EMPTY  [(filter some? (map start-point? area-map))])) (next-move [0 0])))
+
+;; (peek (pop (pop a)))(peek a)
+
+;; (->> [[0 0]]
+;;     (into clojure.lang.PersistentQueue/EMPTY)
+;;     pop
+;;     seq
+;;     )
--- src/advent_of_clerk/day_13.clj
+++ src/advent_of_clerk/day_13.clj
@@ -1,3 +1,71 @@
 ;; # 🎄 Advent of Clerk: Day 13
 (ns advent-of-clerk.day-13
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.string :as str]))
+
+
+(def raw-data (slurp "resources/day_13.txt"))
+;; (def raw-data (slurp "resources/day_13_sample.txt"))
+(def data (read-string (str "[" raw-data "]")))
+
+
+
+(defn compare-lr
+  ([[left right]]
+  (compare-lr left right))
+
+  ([left right]
+  (cond
+    (and (integer? left) (integer? right))
+    (cond (< left right) :left
+          (> left right) :right
+          :else :equal)
+
+    (integer? left)
+    (compare-lr [left] right)
+
+    (integer? right)
+    (compare-lr left [right])
+
+    (and (seqable? left) (seqable? right))
+    (loop [i 0]
+      (cond
+        (and (= (count left) (count right))
+             (every? #{:equal} (map compare-lr left right))) :equal
+        (= i (count left)) :left
+        (= i (count right)) :right
+        :else
+        (let [left-right (compare-lr (nth left i) (nth right i))]
+          (if (= :equal left-right)
+            (recur (inc i))
+            left-right))))
+    :else :equal)))
+
+
+;; ### Puzzle 1. Filter valid packet data
+(defn p1 [input]
+  (->> input
+       (partition 2)
+       (map compare-lr)
+       (map-indexed vector)                 ;; index results
+       (filter (comp #(= :left %) second))  ;; get only lefts
+       (map first)                          ;; take index
+       (map inc)                            ;; 1-indexed
+       (apply +)))
+
+(p1 data)
+(assert (= 5013 (p1 data)))
+
+
+;; ### Puzzle 2. Find decoder key
+(defn p2 [input]
+  (let [sort-keys {:left -1 :right 1 :equal 0}
+        sorter #((compare-lr %1 %2) sort-keys)
+        sorted (->> (conj input [[2]] [[6]])
+                    (sort sorter))]
+    (* (inc (.indexOf sorted [[2]]))
+       (inc (.indexOf sorted [[6]])))))
+
+(p2 data)
+
+(assert (= 25038 (p2 data)))
